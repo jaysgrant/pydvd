28c0816f8869875b212c7f8fc108f76fd2c63b34
Jason Grant <jgrant1975@gmail.com>
2018-11-04 22:22:11 -0500

    DB creation logic changes

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import errno
 import sqlite3
 from configparser import ConfigParser
 
-
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config/config.ini')
+
+def create_dir(subdir):
+    if not os.path.exists(subdir):
+        os.makedirs(subdir)
 
 
 def get_settings(config_path=CONFIG_PATH):
@@ -21,16 +25,28 @@ def db_connect():
     return con
 
 
-con = db_connect()
-cur = con.cursor()
+try:
+    subdir = os.path.join(os.path.dirname(__file__), 'data')
+    create_dir(subdir)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
 
-
-sql_create_film_inv_table = """ CREATE TABLE IF NOT EXISTS film_inv (
+try:
+    con = db_connect()
+    cur = con.cursor()
+    sql_create_film_inv_table = """ CREATE TABLE IF NOT EXISTS film_inv (
                                         film_id integer PRIMARY KEY AUTOINCREMENT UNIQUE Not NULL, 
                                         film_name varchar Not NULL,
                                         film_genre varchar,
                                         date_added datetime
                                     ); """
-
-cur.execute(sql_create_film_inv_table)
+    cur.execute(sql_create_film_inv_table)
+except sqlite3.Error as e:
+    print(e)
+except Exception as e:
+    print(e)
+finally:
+    if con:
+        con.close()
 
