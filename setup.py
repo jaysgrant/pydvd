05d01874e661771f2a078a4e1e29189d7f5f950b
Jason Grant <jgrant1975@gmail.com>
2019-03-16 13:57:49 -0400

    setup base packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='PyDVD',
+    version='0.1.3',
+    packages=['pydvd',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
